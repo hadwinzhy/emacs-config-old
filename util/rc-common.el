9617d4c1f756c3e8a97ef849daeae821e75b02bf
Hadwin Zheng <hadwinzhy@gmail.com>
2013-02-25 21:50:42 +0800

    add word-count function

--- util/rc-common.el
+++ util/rc-common.el
@@ -93,3 +93,25 @@
 (setq mouse-yank-at-point t);支持中键粘贴
 (setq x-select-enable-clipboard t);支持emacs和外部程序的粘贴
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;word count;;;;;;;;;;;;;;;;;;;
+(defun my-word-count()
+  (interactive)
+  (let ((beg (point-min)) (end (point-max))
+        (eng 0) (non-eng 0))
+    (if mark-active
+        (setq beg (region-beginning)
+              end (region-end)))
+    (save-excursion
+      (goto-char beg)
+      (while (< (point) end)
+        (cond ((not (equal (car (syntax-after (point))) 2))
+               (forward-char))
+              ((< (char-after) 128)     
+               (progn
+                 (setq eng (1+ eng))
+                 (forward-word)))
+              (t
+               (setq non-eng (1+ non-eng))
+               (forward-char)))))
+    (message "English words: %d\nNon-English characters: %d"
+             eng non-eng)))
