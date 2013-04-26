7b0647c461d065231f56afa2d3b71b4771336be4
hadwinzhy <hadwinzhy@gmail.com>
2013-04-26 16:43:01 +0800

    add markdown function for .md & .text file

--- lang/lang-markdown.el
+++ lang/lang-markdown.el
@@ -1,6 +1,6 @@
 (provide 'lang-markdown)
 (autoload 'markdown-mode "markdown-mode.el" 
   "Major mode for editing Markdown files" t)
-(setq auto-mode-alist 
-      (cons '("\\.markdown" . markdown-mode) auto-mode-alist))
-;      (cons '("\\.text" . markdown-mode) auto-mode-alist))
+(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
