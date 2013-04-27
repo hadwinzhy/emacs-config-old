4f38c1777fbd0086fc7eabd02d02ae2530a5db7c
hadwinzhy <hadwinzhy@gmail.com>
2013-04-27 17:30:51 +0800

    add org-mode

--- lang/lang-orgmode.el
+++ lang/lang-orgmode.el
@@ -1,31 +1,20 @@
 (provide 'lang-orgmode)
 
-(setq load-path (cons "/usr/share/org-mode/lisp/" load-path))
+(require 'org-install)
 
-(defun save-then-publish ()
-  (interactive)
-  (save-buffer)
-  (org-save-all-org-buffers)
-  (org-publish-current-project))
+(setq load-path (cons "/local/code/github/emacs-config/3rd-party/org-mode/lisp" load-path))
 
-(setq org-publish-project-alist
-      '(("blog-org" .  (:base-directory "/local/code/github/hadwinzhy.github.com/source/org_posts/"
-                                        :base-extension "org"
-                                        :publishing-directory "/local/code/github/hadwinzhy.github.com/source/_posts/"
-                                        :sub-superscript ""
-                                        :recursive t
-                                        :publishing-function org-publish-org-to-octopress
-                                        :headline-levels 4
-                                        :html-extension "markdown"
-                                        :octopress-extension "markdown"
-                                        :body-only t))
-        ("blog-extra" . (:base-directory "/local/code/github/hadwinzhy.github.com/source/org_posts/"
-                                         :publishing-directory "~/git/blog/source/"
-                                         :base-extension "css\\|pdf\\|png\\|jpg\\|gif\\|svg"
-                                         :publishing-function org-publish-attachment
-                                         :recursive t
-                                         :author nil
-                                         ))
-        ("blog" . (:components ("blog-org" "blog-extra")))
-        ))
+ (setq org-agenda-files (list "/local/data/org-mode/newgtd.org"
+                            "/local/data/org-mode/journal.org"
+                             "/local/data/org-mode/birthday.org"))
 
+(setq org-todo-keywords
+    '((sequence "TODO(t)" "WAIT(w)" "|" "DONE(d@/!)"  "CANCELED(c@/!)" )
+     ))    
+
+(defun org-summary-todo (n-done n-not-done)
+      "Switch entry to DONE when all subentries are done, to TODO otherwise."
+      (let (org-log-done org-log-states)   ; turn off logging
+        (org-todo (if (= n-not-done 0) "DONE" "TODO"))))
+    
+    (add-hook 'org-after-todo-statistics-hook 'org-summary-todo)
