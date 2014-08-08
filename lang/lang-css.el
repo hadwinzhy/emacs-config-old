707cee3e57b970fa88e8e285eb6976fff4c2e422
hadwinzhy <hadwinzhy@gmail.com>
2014-08-08 15:33:56 +0800

    add git submodules for haml and scss

--- lang/lang-css.el
+++ lang/lang-css.el
@@ -1,2 +1,12 @@
-(require 'sass-mode)
+;sass
+;(require 'sass-mode)
+
+;scss
+(require 'scss-mode)
+(set 'scss-compile-at-save' nil)
+(autoload 'scss-mode' "scss-mode")
+(add-to-list 'auto-mode-alist '("\\.sass\\'" . scss-mode))
+(add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
+
+
 (provide 'lang-css)
