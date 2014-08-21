f171187f4493804b3087e94097fe7224eab876c1
hadwinzhy <hadwinzhy@gmail.com>
2014-08-21 21:46:10 +0800

    fix sass bug for comment

--- lang/lang-css.el
+++ lang/lang-css.el
@@ -1,11 +1,11 @@
 ;sass
-;(require 'sass-mode)
+(require 'sass-mode)
 
 ;scss
 (require 'scss-mode)
 (set 'scss-compile-at-save' nil)
 (autoload 'scss-mode' "scss-mode")
-(add-to-list 'auto-mode-alist '("\\.sass\\'" . scss-mode))
+;(add-to-list 'auto-mode-alist '("\\.sass\\'" . scss-mode))
 (add-to-list 'auto-mode-alist '("\\.scss\\'" . scss-mode))
 
 
