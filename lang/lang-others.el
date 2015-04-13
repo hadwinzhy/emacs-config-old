c3c5a2b186169a54f2c2abe1217140aeea706303
hadwinzhy <hadwinzhy@gmail.com>
2015-04-13 16:28:36 +0800

    add docker file mode

--- lang/lang-others.el
+++ lang/lang-others.el
@@ -1,5 +1,12 @@
+;;;;;;;;;;JSON
 (require 'json-mode)
 (add-hook 'json-mode-hook
           (lambda ()
             (setq js-indent-level 4)))
+
+;;;;;;;;;;Docker
+(require 'dockerfile-mode)
+(add-to-list 'auto-mode-alist '("Dockerfile\\'" . dockerfile-mode))
+
+;;;;;;;;;;;;;;;;;
 (provide 'lang-others)
