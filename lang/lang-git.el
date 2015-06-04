3d43cfc9e5ca7bd9f599db1715269a27c07a9ce7
hadwinzhy <hadwinzhy@gmail.com>
2015-06-04 12:15:33 +0800

    using new magit

--- lang/lang-git.el
+++ lang/lang-git.el
@@ -1,8 +1,7 @@
-(add-to-list 'load-path "/3rd-party/git-modes")
-(add-to-list 'load-path "/3rd-party/magit")
-(eval-after-load 'info
-  '(progn (info-initialize)
-          (add-to-list 'Info-directory-list "/3rd-party/magit/")))
+;(add-to-list 'load-path "/3rd-party/magit")
+;(eval-after-load 'info
+;  '(progn (info-initialize)
+;          (add-to-list 'Info-directory-list "/3rd-party/magit/")))
 (require 'magit)
 (global-set-key [f9] 'magit-status)
 (provide 'lang-git)
